0a0f7ed5a24c908ca18766be442536ff46745e36
AlexeyPanshin <dispes@mail.ru>
2023-01-27 00:09:32 +0300

    Add project files.

---
+++ Tasks/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public static class Methods
+    {
+        public static void Print(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+        public static void Print(double a)
+        {
+            Console.WriteLine(a);
+        }
+
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 48;
+
+            Methods.Print(number);
+
+        }
+    }
+}
