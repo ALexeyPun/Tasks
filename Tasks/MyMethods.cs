a55dd1fb136bcfcc3aca978c14138c1871743357
AlexeyPanshin <dispes@mail.ru>
2023-01-31 19:36:59 +0300

    Созданы задания.

---
+++ Tasks/MyMethods.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public static class MyMethods
+    {
+        public static void FileHeader(int numberTask)
+        {
+            Console.WriteLine("Домашняя работа Паньшина Алексея.");
+            Console.WriteLine();
+            Console.WriteLine($"Задание {numberTask}.");
+            Console.WriteLine();
+        }
+
+        public static int GetNumberFromConsole(string message)
+        {
+            Console.Write(message);
+            int number = 0;
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out number)) break;
+                Console.Write("Попробуйте снова!");
+            }
+            Console.WriteLine();
+            return number;
+        }
+
+        public static int[,] Create2DArray(int lines, int columns)
+        {
+            int[,] array = new int[lines, columns];
+            return array;
+        }
+
+        public static void FillArrayRandom(int[,] array, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValue, maxValue);
+                }
+            }
+        }
+
+        public static void Print2DArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j  < array.GetLength(1); j ++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static void Sort2DArrayFromMaxToMin(int[,] array)
+        {
+            int maxValue = 0;
+            int lines = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {                    
+                    for (int k = 0; k < columns - 1; k++)
+                    {
+                        if (array[i, k] < array[i, k + 1])
+                        {
+                            maxValue = array[i, k + 1];
+                            array[i, k + 1] = array[i, k];
+                            array[i, k] = maxValue;
+                        }
+                    }                    
+                }
+            }
+
+        }
+
+        public static int GetMinLineIndex(int[,] array2D)
+        {
+            int minValue = 0;
+            int sumElements = 0;
+            int indexMinLine = 0;
+            int[] array = new int[array2D.GetLength(0)];
+
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                sumElements = 0;
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    sumElements += array2D[i, j];
+                }
+                array[i] = sumElements;
+            }
+            minValue = array.Min();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == minValue)
+                {
+                    indexMinLine = i;
+                    break;
+                }
+            }
+            return indexMinLine;
+        }
+
+        public static int[,] CreateMatrix(string message)
+        {
+            Console.WriteLine(message);
+            int[,] matrix = new int[2, 2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix[i,j] = MyMethods.GetNumberFromConsole($"Введите {j + 1} значение {i + 1} строки: ");
+                }
+            }
+            return matrix;
+        }
+
+        public static int[,] ProductOfTwoMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int lines = matrixA.GetLength(0);
+            int columns = matrixA.GetLength(1);
+            int[,] rezultMatrix = new int[lines,columns];
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    for (int k = 0; k < lines; k++)
+                    {
+                        rezultMatrix[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }                   
+                }
+            }
+
+            return rezultMatrix;
+
+        }
+
+        public static void Fill3DArray(int[,,] array)
+        {
+            int axisX = array.GetLength(0);
+            int axisY = array.GetLength(1);
+            int axisZ = array.GetLength(2);
+            int number = 10;
+
+            for (int i = 0; i < axisX; i++)
+            {
+                for (int j = 0; j < axisY; j++)
+                {
+                    for (int k = 0; k < axisZ; k++)
+                    {
+                        array[i, j, k] = number++;
+                                                
+                    }
+                }
+            }
+        }
+
+        public static void Print3DArray(int[,,] array)
+        {
+            Console.WriteLine("Трёхмкрный массив создан.");
+            Console.WriteLine();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        Console.Write($"({i} {j} {k}) = {array[i, j, k]}  ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();                        
+        }
+
+        public static void FillArraySpirally(int[,] array)
+        {
+            int lines = array.GetLength(0) - 1;
+            int columns = array.GetLength(1) - 1;
+            int countElements = (lines + 1) * (columns + 1);
+            int counter = 0;
+            int circle = 0;
+            int i = 0;
+            int j = 0;
+
+            while (counter < countElements)
+            {
+                for (; j <= columns - circle; j++)
+                    array[i, j] = ++counter;
+                j--;
+                i++;
+
+                for (; i <= lines - circle; i++)
+                    array[i, j] = ++counter;
+                i--;
+                j--;
+
+                for (; j >= 0 + circle; j--)
+                    array[i, j] = ++counter;
+                j++;
+                i--;
+                circle++;
+
+                for (; i >= 0 + circle; i--)
+                    array[i, j] = ++counter;
+                i++;
+                j++;
+            }
+                       
+        }
+
+        public static void PrintArraySpirally(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+
+                    if(array[i, j] < 10)
+                        Console.Write("0" + array[i, j] + " ");
+                    else
+                        Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -10,10 +10,122 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            int number = 48;
+            // Задача 54: Задайте двумерный массив.Напишите программу, 
+            // которая упорядочит по убыванию элементы каждой строки двумерного массива.
+            
+            MyMethods.FileHeader(54);
+            Console.WriteLine("Задайте двумерный массив. Напишите программу, \r\nкоторая упорядочит по убыванию элементы каждой строки двумерного массива.");
+            Console.WriteLine();
+            Console.WriteLine("Создание массива.");
+            int lines = MyMethods.GetNumberFromConsole("Введите колличество строк: ");
+            int columns = MyMethods.GetNumberFromConsole("Введите колличество колонок: ");
 
-            Methods.Print(number);
+            int[,] firstArray = MyMethods.Create2DArray(lines, columns);
 
+            Console.WriteLine("Нужны значения для рандомизации.");
+            int minValue = MyMethods.GetNumberFromConsole("Введите минимальное значение: ");
+            int maxValue = MyMethods.GetNumberFromConsole("Введите максимальное значение: ");
+
+            MyMethods.FillArrayRandom(firstArray, minValue, maxValue);
+            MyMethods.Print2DArray(firstArray);
+            
+            MyMethods.Sort2DArrayFromMaxToMin(firstArray);
+            MyMethods.Print2DArray(firstArray);
+                        
+            Console.WriteLine("Программа завершена.");
+            Console.ReadKey();
+
+
+            // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+            // которая будет находить строку с наименьшей суммой элементов.
+
+            Console.Clear();
+            Console.WriteLine("Задание 56.");
+            Console.WriteLine();
+            Console.WriteLine("Задайте прямоугольный двумерный массив. Напишите программу, \r\nкоторая будет находить строку с наименьшей суммой элементов.");
+            Console.WriteLine();
+
+            Console.WriteLine("Создание массива.");
+            lines = MyMethods.GetNumberFromConsole("Введите колличество строк: ");
+            columns = MyMethods.GetNumberFromConsole("Введите колличество колонок: ");
+
+
+            int[,] secondArray = MyMethods.Create2DArray(lines, columns);
+
+            Console.WriteLine("Нужны значения для рандомизации.");
+            minValue = MyMethods.GetNumberFromConsole("Введите минимальное значение: ");
+            maxValue = MyMethods.GetNumberFromConsole("Введите максимальное значение: ");
+
+            MyMethods.FillArrayRandom(secondArray, minValue, maxValue);
+            MyMethods.Print2DArray(secondArray);
+
+            int indexMinVaueLine = MyMethods.GetMinLineIndex(secondArray);
+            Console.WriteLine($"Индекс линии с минимальным значением - {indexMinVaueLine}");
+            Console.WriteLine();
+            Console.WriteLine("Программа завершена.");
+            Console.ReadKey();
+
+
+
+            // Задача 58: Задайте две матрицы.Напишите программу, которая будет находить произведение двух матриц.
+
+            Console.Clear();
+            Console.WriteLine("Задание 58.");
+            Console.WriteLine("Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.");
+            Console.WriteLine();
+
+            int[,] matrixA = MyMethods.CreateMatrix("Создайте матрицу А.");
+            int[,] matrixB = MyMethods.CreateMatrix("Создайте матрицу В.");
+
+            MyMethods.Print2DArray(matrixA);
+            MyMethods.Print2DArray(matrixB);
+
+            int[,] rezultMatrix = MyMethods.ProductOfTwoMatrices(matrixA, matrixB);
+
+            Console.WriteLine("Результат умножения матриц.");
+            Console.WriteLine();
+            MyMethods.Print2DArray(rezultMatrix);
+            Console.WriteLine("Программа завершена.");
+            Console.ReadKey();
+
+
+
+            // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+            // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+            // Массив размером 2 x 2 x 2
+
+            Console.Clear();
+            Console.WriteLine("Задание 60.");
+            Console.WriteLine("Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.");
+            Console.WriteLine("Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.");
+            Console.WriteLine();
+
+            int[,,] array3D = new int[2, 2, 2];
+
+            MyMethods.Fill3DArray(array3D);
+
+            MyMethods.Print3DArray(array3D);
+            Console.WriteLine("Программа завершена.");
+            Console.ReadKey();
+
+
+            // Задача 62.Напишите программу, которая заполнит спирально массив 4 на 4.
+
+            Console.Clear();
+            Console.WriteLine("Задание 62.");
+            Console.WriteLine("Напишите программу, которая заполнит спирально массив 4 на 4.");
+            Console.WriteLine();
+
+            lines = MyMethods.GetNumberFromConsole("Введите количество линий: ");
+            columns = MyMethods.GetNumberFromConsole("Введите количество линий: ");
+
+            int[,] array = MyMethods.Create2DArray(lines, columns);
+
+            MyMethods.FillArraySpirally(array);
+            MyMethods.PrintArraySpirally(array);
+
+            Console.ReadKey();
+            
         }
     }
 }
